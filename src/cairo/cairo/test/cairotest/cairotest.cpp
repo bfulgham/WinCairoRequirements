7763d368a713fb503f8ed7e07eec192833efa9bc
Brent Fulgham <bfulgham@gmail.com>
2012-06-24 21:02:55 -0700

    Try not to starve the CPU when running the test.  Clean up the code a tiny bit and try to add some attributions.

--- src/cairo/cairo/test/cairotest/cairotest.cpp
+++ src/cairo/cairo/test/cairotest/cairotest.cpp
@@ -1,4 +1,14 @@
-// cairotest.cpp : Defines the entry point for the console application.
+// cairotest.cpp 
+//
+// Simple test harness used to confirm Cairo function under Windows.
+//
+// * Uses code from Andrew Lim (danteshamest@gmail.com) from his example program
+//   (see windrealm.com) for the GDI routines.
+//
+// * Uses code and suggestions from 
+//   http://www.nullterminator.net/opengl32.html for OpenGL/WGL stuff.
+//
+// * Also uses the gtkmm cairo clock example code.
 //
 
 #include "stdafx.h"
@@ -14,6 +24,7 @@
 #include <wininet.h>
 
 #include <windows.h>
+#define _USE_MATH_DEFINES
 #include <cmath>
 #include "cairo/cairo-gl.h"
 #include "cairo/cairo-win32.h"
@@ -23,8 +34,7 @@
 HINSTANCE hInst;                                // current instance
 HWND hMainWnd;
 WNDPROC DefEditProc = 0;
-TCHAR szTitle[MAX_LOADSTRING] = _T ("WinLauncher");                    // The title bar text
-TCHAR szWindowClass[MAX_LOADSTRING] =  _T ("WINLAUNCHER");            // the main window class name
+TCHAR szWindowClass[MAX_LOADSTRING] =  _T ("WIN_CAIRO_TEST");            // the main window class name
 HDC dc;
 HGLRC rc;
 int LastUpdate = 0;
@@ -57,21 +67,18 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpstrCm
     InitCtrlEx.dwICC  = 0x00004000; //ICC_STANDARD_CLASSES;
     InitCommonControlsEx(&InitCtrlEx);
 
-    // Initialize global strings
-    //LoadString(hInst, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    //LoadString(hInst, IDC_WINLAUNCHER, szWindowClass, MAX_LOADSTRING);
     MyRegisterClass(hInst);
 
     // Init COM
     OleInitialize(NULL);
 
-    hMainWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+    hMainWnd = CreateWindow(szWindowClass, _T ("Cairo Test Harness"), WS_OVERLAPPEDWINDOW,
                    CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, 0, 0, hInst, 0);
 
     if (!hMainWnd)
         return FALSE;
 
-    SetClientSize(hMainWnd, 256, 256);
+    SetClientSize(hMainWnd, height, width);
 
     dc = ::GetDC (hMainWnd);
 
@@ -80,7 +87,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpstrCm
  
     // set the pixel format for the DC
     ZeroMemory( &pfd, sizeof( pfd ) );
-    pfd.nSize = sizeof( pfd );
+    pfd.nSize = sizeof (pfd);
     pfd.nVersion = 1;
     pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL |
                   PFD_DOUBLEBUFFER;
@@ -88,27 +95,27 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpstrCm
     pfd.cColorBits = 24;
     pfd.cDepthBits = 16;
     pfd.iLayerType = PFD_MAIN_PLANE;
-    iFormat = ChoosePixelFormat( dc, &pfd );
-    SetPixelFormat( dc, iFormat, &pfd );
+    iFormat = ChoosePixelFormat(dc, &pfd);
+    SetPixelFormat(dc, iFormat, &pfd);
  
     rc = wglCreateContext(dc);
  
-    cairo_device_t *device = cairo_wgl_device_create(rc);
+    cairo_device_t* device = cairo_wgl_device_create(rc);
  
-    if(cairo_device_status(device) != CAIRO_STATUS_SUCCESS)
-        printf("cairo device failed with %s\n", cairo_status_to_string(cairo_device_status(device)));
+    if (cairo_device_status(device) != CAIRO_STATUS_SUCCESS)
+       printf("cairo device failed with %s\n", cairo_status_to_string(cairo_device_status(device)));
  
     surface = cairo_gl_surface_create_for_dc(device, dc, width, height);
  
-    if(cairo_surface_status(surface) != CAIRO_STATUS_SUCCESS)
+    if (cairo_surface_status(surface) != CAIRO_STATUS_SUCCESS)
         printf("cairo surface failed with %s\n", cairo_status_to_string(cairo_surface_status(surface)));
  
     cairo_device_destroy(device);
 
     cr = cairo_create (surface);
  
-    if(cairo_status(cr) != CAIRO_STATUS_SUCCESS)
-        printf("cairo failed with %s\n", cairo_status_to_string(cairo_status(cr)));
+    if (cairo_status(cr) != CAIRO_STATUS_SUCCESS)
+       printf("cairo failed with %s\n", cairo_status_to_string(cairo_status(cr)));
  
     ShowWindow(hMainWnd, SW_SHOWNORMAL);
 
@@ -117,24 +124,28 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpstrCm
 
     wglMakeCurrent(dc, rc);
 
-        while (running)
+    while (running)
+    {
+        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
         {
-                if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
-                {
-                        if (msg.message == WM_QUIT)
-                        {
-                                running = false;
-                        }
-                        else
-                        {
-                                TranslateMessage( &msg );
-                                DispatchMessage( &msg );
-                        }
-                }
-            render();
- 
-            SwapBuffers(dc);
+            if (msg.message == WM_QUIT)
+            {
+                running = false;
+            }
+            else
+            {
+                TranslateMessage (&msg);
+                DispatchMessage (&msg);
+            }
         }
+        else
+        {
+            render();
+            SwapBuffers(dc);
+            ::Sleep (100);
+        }
+    }
+
 exit:
 #ifdef _CRTDBG_MAP_ALLOC
     _CrtDumpMemoryLeaks();
@@ -176,48 +187,6 @@ LRESULT OnClose(HWND hwnd, WPARAM wParam, LPARAM lParam)
   PostQuitMessage(0);
   return 0;
 }
-
-LRESULT OnPaint(HWND hwnd, WPARAM wParam, LPARAM lParam);
-
-/**
-  Handles our window's messages.
-*/
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-  switch (message)
-  {
-    //case WM_PAINT: return OnPaint(hWnd, wParam, lParam);
-    case WM_CLOSE: 
-        return OnClose(hWnd, wParam, lParam);
-    case WM_DESTROY:
-        /*cairo_destroy(cr);
-        cairo_surface_destroy(surface);
- 
-        wglMakeCurrent( NULL, NULL );
-        wglDeleteContext( rc );*/
- 
-        ReleaseDC(hWnd, dc);
-        PostQuitMessage (0);
-        break;
-
-    default:
-        return DefWindowProc(hWnd,message,wParam,lParam);
-  }
-
-  return DefWindowProc(hWnd,message,wParam,lParam);
-}
-
-/**
-cairo-gdi-demo.cpp
-
-Demonstrates how to get Cairo Graphics working with the Windows API and GDI.
-                     
-Author: Andrew Lim
-Email:  danteshamest@gmail.com
-Site: windrealm.com
-*/
-
-#define M_PI 3.141
 
 /**
   Gradient demonstration.
@@ -285,107 +254,134 @@ LRESULT OnPaint(HWND hwnd, WPARAM wParam, LPARAM lParam)
   return 0 ;
 }
 
+/**
+  Handles our window's messages.
+*/
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+  switch (message)
+  {
+    //case WM_PAINT: return OnPaint(hWnd, wParam, lParam);
+    case WM_CLOSE: 
+        return OnClose(hWnd, wParam, lParam);
+    case WM_DESTROY:
+        /*cairo_destroy(cr);
+        cairo_surface_destroy(surface);
+ 
+        wglMakeCurrent( NULL, NULL );
+        wglDeleteContext( rc );*/
+ 
+        ReleaseDC(hWnd, dc);
+        PostQuitMessage (0);
+        break;
+
+    default:
+        return DefWindowProc(hWnd,message,wParam,lParam);
+  }
+
+  return DefWindowProc(hWnd,message,wParam,lParam);
+}
+
 void render()
 {
-           if( GetTickCount() - LastUpdate > 1000)
-            {
-                    //std::cout << "Fps: " << Frames / ((GetTickCount() - LastUpdate) / 1000.f) << "\n";
- 
-                    LastUpdate = GetTickCount();
-                    Frames = 0;
-            }
- 
-            cairo_identity_matrix(cr);
- 
-            double m_radius = 0.42;
-            double m_line_width = 0.05;
- 
-            cairo_scale(cr, width, height);
-            cairo_translate(cr, 0.5, 0.5);
-            cairo_set_line_width(cr, m_line_width);
- 
-            cairo_save(cr);
-            cairo_set_source_rgba(cr, 0.337, 0.612, 0.117, 0.9);   // green
-            cairo_paint(cr);
-            cairo_restore(cr);
-            cairo_arc(cr, 0, 0, m_radius, 0, 2 * M_PI);
-            cairo_save(cr);
-            cairo_set_source_rgba(cr, 1.0, 1.0, 1.0, 0.8);
-            cairo_fill_preserve(cr);
-            cairo_restore(cr);
-            cairo_stroke_preserve(cr);
-            cairo_clip(cr);
- 
-            //clock ticks
-            for (int i = 0; i < 12; i++)
-            {
-                double inset = 0.05;
- 
-                cairo_save(cr);
-                cairo_set_line_cap(cr, CAIRO_LINE_CAP_ROUND);
- 
-                if(i % 3 != 0)
-                {
-                    inset *= 0.8;
-                    cairo_set_line_width(cr, 0.03);
-                }
- 
-                cairo_move_to(cr,
-                        (m_radius - inset) * cos (i * M_PI / 6),
-                        (m_radius - inset) * sin (i * M_PI / 6));
-                cairo_line_to (cr,
-                        m_radius * cos (i * M_PI / 6),
-                        m_radius * sin (i * M_PI / 6));
-                cairo_stroke(cr);
-                cairo_restore(cr); /* stack-pen-size */
-            }
- 
-            // store the current time
-            SYSTEMTIME time;
-            GetLocalTime(&time);
- 
-            // compute the angles of the indicators of our clock
-            double minutes = time.wMinute * M_PI / 30;
-            double hours = time.wHour * M_PI / 6;
-            double seconds= ((double)time.wSecond + (double)time.wMilliseconds / 1000) * M_PI / 30;
- 
-            cairo_save(cr);
-            cairo_set_line_cap(cr, CAIRO_LINE_CAP_ROUND);
- 
-            // draw the seconds hand
-            cairo_save(cr);
-            cairo_set_line_width(cr, m_line_width / 3);
-            cairo_set_source_rgba(cr, 0.7, 0.7, 0.7, 0.8); // gray
-            cairo_move_to(cr, 0, 0);
-            cairo_line_to(cr, sin(seconds) * (m_radius * 0.9),
-                    -cos(seconds) * (m_radius * 0.9));
-            cairo_stroke(cr);
-            cairo_restore(cr);
- 
-            // draw the minutes hand
-            cairo_set_source_rgba(cr, 0.117, 0.337, 0.612, 0.9);   // blue
-            cairo_move_to(cr, 0, 0);
-            cairo_line_to(cr, sin(minutes + seconds / 60) * (m_radius * 0.8),
-                    -cos(minutes + seconds / 60) * (m_radius * 0.8));
-            cairo_stroke(cr);
- 
-            // draw the hours hand
-            cairo_set_source_rgba(cr, 0.337, 0.612, 0.117, 0.9);   // green
-            cairo_move_to(cr, 0, 0);
-            cairo_line_to(cr, sin(hours + minutes / 12.0) * (m_radius * 0.5),
-                    -cos(hours + minutes / 12.0) * (m_radius * 0.5));
-            cairo_stroke(cr);
-            cairo_restore(cr);
- 
-            // draw a little dot in the middle
-            cairo_arc(cr, 0, 0, m_line_width / 3.0, 0, 2 * M_PI);
-            cairo_fill(cr);
- 
-            cairo_surface_flush(surface);
- 
-            if(cairo_status(cr) != CAIRO_STATUS_SUCCESS)
-                printf("render failed with %s\n", cairo_status_to_string(cairo_status(cr)));
- 
-                        Frames++;
- 
+    if (GetTickCount() - LastUpdate > 1000)
+    {
+        //std::cout << "Fps: " << Frames / ((GetTickCount() - LastUpdate) / 1000.f) << "\n";
+
+        LastUpdate = GetTickCount();
+        Frames = 0;
+    }
+
+    cairo_identity_matrix(cr);
+
+    double m_radius = 0.42;
+    double m_line_width = 0.05;
+
+    cairo_scale(cr, width, height);
+    cairo_translate(cr, 0.5, 0.5);
+    cairo_set_line_width(cr, m_line_width);
+
+    cairo_save(cr);
+    cairo_set_source_rgba(cr, 0.337, 0.612, 0.117, 0.9);   // green
+    cairo_paint(cr);
+    cairo_restore(cr);
+    cairo_arc(cr, 0, 0, m_radius, 0, 2 * M_PI);
+    cairo_save(cr);
+    cairo_set_source_rgba(cr, 1.0, 1.0, 1.0, 0.8);
+    cairo_fill_preserve(cr);
+    cairo_restore(cr);
+    cairo_stroke_preserve(cr);
+    cairo_clip(cr);
+
+    //clock ticks
+    for (int i = 0; i < 12; i++)
+    {
+        double inset = 0.05;
+
+        cairo_save(cr);
+        cairo_set_line_cap(cr, CAIRO_LINE_CAP_ROUND);
+
+        if(i % 3 != 0)
+        {
+            inset *= 0.8;
+            cairo_set_line_width(cr, 0.03);
+        }
+
+        cairo_move_to(cr,
+                (m_radius - inset) * cos (i * M_PI / 6),
+                (m_radius - inset) * sin (i * M_PI / 6));
+        cairo_line_to (cr,
+                m_radius * cos (i * M_PI / 6),
+                m_radius * sin (i * M_PI / 6));
+        cairo_stroke(cr);
+        cairo_restore(cr); /* stack-pen-size */
+    }
+
+    // store the current time
+    SYSTEMTIME time;
+    GetLocalTime(&time);
+
+    // compute the angles of the indicators of our clock
+    double minutes = time.wMinute * M_PI / 30;
+    double hours = time.wHour * M_PI / 6;
+    double seconds= ((double)time.wSecond + (double)time.wMilliseconds / 1000) * M_PI / 30;
+
+    cairo_save(cr);
+    cairo_set_line_cap(cr, CAIRO_LINE_CAP_ROUND);
+
+    // draw the seconds hand
+    cairo_save(cr);
+    cairo_set_line_width(cr, m_line_width / 3);
+    cairo_set_source_rgba(cr, 0.7, 0.7, 0.7, 0.8); // gray
+    cairo_move_to(cr, 0, 0);
+    cairo_line_to(cr, sin(seconds) * (m_radius * 0.9),
+            -cos(seconds) * (m_radius * 0.9));
+    cairo_stroke(cr);
+    cairo_restore(cr);
+
+    // draw the minutes hand
+    cairo_set_source_rgba(cr, 0.117, 0.337, 0.612, 0.9);   // blue
+    cairo_move_to(cr, 0, 0);
+    cairo_line_to(cr, sin(minutes + seconds / 60) * (m_radius * 0.8),
+            -cos(minutes + seconds / 60) * (m_radius * 0.8));
+    cairo_stroke(cr);
+
+    // draw the hours hand
+    cairo_set_source_rgba(cr, 0.337, 0.612, 0.117, 0.9);   // green
+    cairo_move_to(cr, 0, 0);
+    cairo_line_to(cr, sin(hours + minutes / 12.0) * (m_radius * 0.5),
+            -cos(hours + minutes / 12.0) * (m_radius * 0.5));
+    cairo_stroke(cr);
+    cairo_restore(cr);
+
+    // draw a little dot in the middle
+    cairo_arc(cr, 0, 0, m_line_width / 3.0, 0, 2 * M_PI);
+    cairo_fill(cr);
+
+    cairo_surface_flush(surface);
+
+    if (cairo_status(cr) != CAIRO_STATUS_SUCCESS)
+       printf("render failed with %s\n", cairo_status_to_string(cairo_status(cr)));
+
+    Frames++;
 }
